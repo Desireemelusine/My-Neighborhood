35a1ac3cf7d33aacc7751bd41424601b7e697055
Desireemelusine <demelusine@gmail.com>
2018-09-03 12:27:04 +0100

    googleMaps + side bar -perfect

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-$("#menu-toggle").click(function(e) {
-  e.preventDefault();
-  $("#wrapper").toogleClass("toggled");
-})
+var map;
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 38.715278, lng: -9.166944},
+    zoom: 16
+  });
+}
